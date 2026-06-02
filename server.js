b412f62ba3f1958e6a39bf14d7b1bcf742e3f32f
code5tomono11-wp <code.5.tomo.no.11@gmail.com>
2026-06-03 08:23:10 +0900

    first release

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+
+    console.log("接続:", socket.id);
+
+    socket.on("createRoom", () => {
+
+        const code =
+            Math.floor(100000 + Math.random() * 900000)
+            .toString();
+
+        rooms[code] = {
+            players: [socket.id]
+        };
+
+        socket.join(code);
+
+        socket.emit("roomCreated", code);
+
+        console.log("ルーム作成:", code);
+    });
+
+    socket.on("joinRoom", (code) => {
+
+        if (!rooms[code]) {
+            socket.emit(
+                "errorMessage",
+                "ルームが存在しません"
+            );
+            return;
+        }
+
+        rooms[code].players.push(socket.id);
+
+        socket.join(code);
+
+        io.to(code).emit("battleReady");
+
+        console.log("ルーム参加:", code);
+    });
+
+});
+
+server.listen(3000, "0.0.0.0", () => {
+    console.log("サーバー起動");
+});
